9cc2d01fbcfadf911caf21599efd7fba8558f6e1
stefan Haslinger <mail@stefan-haslinger.at>
2019-08-06 17:36:42 +0200

    minimal audio comment working

--- lib/widgets/audio_comment.dart
+++ lib/widgets/audio_comment.dart
@@ -4,6 +4,8 @@ import 'package:recomendo/screens/recommendation_detail.dart';
 import 'package:audio_recorder/audio_recorder.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:uuid/uuid.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class AudioComment extends StatelessWidget {
   const AudioComment({Key key, @required this.recommendation, this.state})
@@ -12,35 +14,46 @@ class AudioComment extends StatelessWidget {
   final Recommendation recommendation;
   final RecommendationDetailState state;
 
+
   @override
   Widget build(BuildContext context) {
+    AudioPlayer audioPlayer = AudioPlayer();
     TextStyle textStyle = Theme.of(context).textTheme.title;
 
-    return Row(children: <Widget>[
-      Expanded(child: Text(recommendation.audioComment ?? "No recording yet.")),
-      Expanded(
-          child: RaisedButton(
-              onPressed: recordComment, child: Text("Record Comment")))
+    return ListView(children: <Widget>[
+      Text(recommendation.audioComment ?? "No recording yet."),
+      RaisedButton(onPressed: recordComment, child: Text("Start Recording")),
+      RaisedButton(onPressed: stopRecording, child: Text("Stop Recording")),
+      RaisedButton(onPressed: () => play(path: recommendation.audioComment),
+                   child: Text("Play")),
     ]);
   }
 
   Future recordComment() async {
     bool _hasPermissions = await AudioRecorder.hasPermissions;
 
-    if(_hasPermissions == false){
-      print("I am missing permissions!");
+    if (_hasPermissions == false) {
       await PermissionHandler().requestPermissions([PermissionGroup.storage]);
-      PermissionStatus storage = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
-      print("storage ${storage}");
-      await PermissionHandler().requestPermissions([PermissionGroup.microphone]);
-      PermissionStatus microphone = await PermissionHandler().checkPermissionStatus(PermissionGroup.microphone);
-      print("microphone ${microphone}");
+      PermissionStatus storage = await PermissionHandler()
+          .checkPermissionStatus(PermissionGroup.storage);
+      await PermissionHandler()
+          .requestPermissions([PermissionGroup.microphone]);
+      PermissionStatus microphone = await PermissionHandler()
+          .checkPermissionStatus(PermissionGroup.microphone);
     };
 
+    var uuid = Uuid();
+
+    final directory = await getApplicationDocumentsDirectory();
+    var fullPath = directory.path;
+    var endPosition = fullPath.lastIndexOf('/');
+
     await AudioRecorder.start(
-        path: await _localPath,
-        audioOutputFormat: AudioOutputFormat.AAC
-    );
+        path: await fullPath.substring(0, endPosition) + "/" + uuid.v1(),
+        audioOutputFormat: AudioOutputFormat.AAC);
+  }
+
+  Future stopRecording() async {
     Recording recording = await AudioRecorder.stop();
 
     state.setState(() {
@@ -48,9 +61,10 @@ class AudioComment extends StatelessWidget {
     });
   }
 
-
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
+  Future play({String path}) async{
+    if(path != null) {
+      AudioPlayer audioPlayer = AudioPlayer();
+      await audioPlayer.play(path, isLocal: true);
+    }
   }
 }
